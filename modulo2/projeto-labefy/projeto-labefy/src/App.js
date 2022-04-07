241e01886944f19e2690773df96e33a1fad2dbc0
Pablo Gomes <pablogomes_bm@hotmail.com>
2022-04-07 17:04:22 -0300

    inicio do projeto

---
+++ modulo2/projeto-labefy/projeto-labefy/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import axios from 'axios'
+
+export default class App extends React.Component{
+  render() {
+    return (
+      <div>
+        <h1>Labefy sua musica preferida vc encontra aqui!</h1>
+
+      </div>
+    );
+
+  }
+}
