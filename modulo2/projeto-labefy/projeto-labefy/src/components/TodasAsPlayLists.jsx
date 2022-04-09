fbb9a701a6962d3cb8d1ac90fa4751aff3ec99eb
Pablo Gomes <pablogomes_bm@hotmail.com>
2022-04-09 16:20:30 -0300

    hover nas playlists

--- modulo2/projeto-labefy/projeto-labefy/src/components/TodasAsPlayLists.jsx
+++ modulo2/projeto-labefy/projeto-labefy/src/components/TodasAsPlayLists.jsx
@@ -14,6 +14,9 @@ justify-content: space-between;
 padding: 10px;
 align-items: center;
 margin:10px;
+&:hover{
+    background-color: rgb(22 22 22);
+}
 
 `
 
@@ -21,19 +24,19 @@ margin:10px;
 export default class TodasAsPlayLists extends React.Component {
     state = {
         playLists: [],
-        tracks:[],
-        nameInput:"",
-        artistInput:"",
-        urlInput:""
+        tracks: [],
+        nameInput: "",
+        artistInput: "",
+        urlInput: ""
     }
-  
+
     //Requisição para pegar todas as playlists
     getAllPlayLists = () => {
         const url = "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists"
         axios
             .get(url, headers)
             .then((res) => {
-               
+
                 this.setState({ playLists: res.data.result.list })
             })
             .catch((err) => {
@@ -45,15 +48,9 @@ export default class TodasAsPlayLists extends React.Component {
         this.getAllPlayLists()
     }
     //função do botao veja mais
-    onClickPlaylistDetalhes=()=>{
-        const detalhesPlaylist = this.state.tracks.map((musica)=>{
-            
-            return <div>{musica.track}</div>
-        })
-       
-    }
+
     //requisição para adicionar musicas a playlist
-   
+
     postAddTrackToPlaylist = (id) => {
         const url = `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}/tracks`
         const body = {
@@ -71,53 +68,54 @@ export default class TodasAsPlayLists extends React.Component {
             })
 
     }
-oChangenameInput =(event)=>{
-    this.setState({
-        nameInput: event.target.value
-    })
-}
-oChangeArtistInput =(event)=>{
-   this.setState({artistInput: event.target.value}) 
-}
-oChangeUrlInput =(event)=>{
-    this.setState({urlInput: event.target.value})
-}
-componentDidUpdate() {
 
-    this.postAddTrackToPlaylist()
+    oChangenameInput = (event) => {
+        this.setState({
+            nameInput: event.target.value
+        })
+    }
+    oChangeArtistInput = (event) => {
+        this.setState({ artistInput: event.target.value })
+    }
+    oChangeUrlInput = (event) => {
+        this.setState({ urlInput: event.target.value })
+    }
+    componentDidUpdate() {
 
-}
+        this.postAddTrackToPlaylist()
+
+    }
 
 
     //requisição para pegar todas as musicas da playlist
-   
+
     getPlayListTracks = (id) => {
         const url = `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}/tracks`
         axios
             .get(url, headers)
             .then((res) => {
-                this.setState({tracks: res.data.result.tracks})
+                this.setState({ tracks: res.data.result.tracks })
             })
             .catch((err) => {
-             console.log(err)
+                console.log(err)
             })
 
     }
 
-    
+
 
     // requisção parfa deletar playlists
-    deletePlaylist=(id)=>{
-    const url = `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}`
-    axios
-    .delete(url, headers)
-    .then((res)=>{
-       alert("A Playlist foi deletada!")
-       this.getAllPlayLists()
-    })
-    .catch((err)=>{
-        console.log(err)
-    })
+    deletePlaylist = (id) => {
+        const url = `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}`
+        axios
+            .delete(url, headers)
+            .then((res) => {
+                alert("A Playlist foi deletada!")
+                this.getAllPlayLists()
+            })
+            .catch((err) => {
+                console.log(err)
+            })
     }
 
 
@@ -133,9 +131,9 @@ componentDidUpdate() {
         const allPlayLists = this.state.playLists.map((playlist) => {
             return <CardPlayList key={playlist.id}>
                 {playlist.name}
-               
-                <button onClick={()=>this.deletePlaylist(playlist.id)}>Delete Playlist</button>
-              
+
+                <button onClick={() => this.deletePlaylist(playlist.id)}>Delete Playlist</button>
+
             </CardPlayList>
         })
 
@@ -143,22 +141,22 @@ componentDidUpdate() {
             <div>
                 {allPlayLists}
                 <div>
-               
-               <input type="text"
-               placeholder='Nome da Música'
-               value={this.state.nameInput}
-               onChange={this.oChangenameInput}
-               />
-               <input type="text"
-               placeholder='Astista'
-               value={this.state.artistInput}
-               onChange={this.oChangeArtistInput}/>
-               <input type="text"
-               placeholder='Url'
-               value={this.state.urlInput}
-               onChange={this.oChangeUrlInput}/>
-               <button onClick={this.postAddTrackToPlaylist}>Adicionar à playlist</button>
-           </div>
+
+                    <input type="text"
+                        placeholder='Nome da Música'
+                        value={this.state.nameInput}
+                        onChange={this.oChangenameInput}
+                    />
+                    <input type="text"
+                        placeholder='Astista'
+                        value={this.state.artistInput}
+                        onChange={this.oChangeArtistInput} />
+                    <input type="text"
+                        placeholder='Url'
+                        value={this.state.urlInput}
+                        onChange={this.oChangeUrlInput} />
+                    <button onClick={this.postAddTrackToPlaylist}>Adicionar à playlist</button>
+                </div>
 
             </div>
         )
