eeaa3fb1790fb63b2d7aa865b60b5d259dcccecd
Pablo Gomes <pablogomes_bm@hotmail.com>
2022-07-11 18:36:14 -0300

    exercicios de testes automatizados

---
+++ modulo5/testes-automatizados/testes-automatizados/src/index.ts
@@ -0,0 +1,33 @@
+import knex from 'knex'
+import { config } from 'dotenv'
+
+config()
+
+export const isEven = (integer: number): any => { }
+
+export const connection = knex({
+   client: "mysql",
+   connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_SCHEMA,
+      port: 3306,
+      multipleStatements: true
+   },
+});
+
+export interface User {
+	name: string
+	balance: number
+}
+
+export function performPurchase(user: User, value: number): User | undefined {
+	if(user.balance >= value) {
+		return {
+			...user,
+			balance: user.balance - value		
+		}
+	}
+	return undefined
+}
---
+++ modulo5/testes-automatizados/testes-automatizados/tests/index.test.ts
@@ -0,0 +1,48 @@
+import { performPurchase, User } from "../src"
+
+describe("Esse é o meu primeiro teste", ()=>{
+    test("Esse teste vai verificar uma string", ()=>{
+        const nome: string = "rodrigo"
+
+        expect(nome).toBe("rodrigo")
+    })
+})
+
+test("Testing balance greater than value", () => {
+	const user: User = {
+		name: "Astrodev",
+		balance: 100
+	}
+
+	const result = performPurchase(user, 40)
+	
+	expect(result).toEqual({
+		name: "Astrodev",
+		balance: 60
+	})
+})
+
+test("Testing balance greater than value", () => {
+	const user: User = {
+		name: "Astrodev",
+		balance: 50
+	}
+
+	const result = performPurchase(user, 50)
+	
+	expect(result).toEqual({
+		...user,
+		balance: 0
+	})
+})
+
+test("Testing balance greater than value", () => {
+	const user: User = {
+		name: "Astrodev",
+		balance: 30
+	}
+
+	const result = performPurchase(user, 50)
+	
+	expect(result).not.toBeDefined()
+})
