34589c105bcd60e9be6cff92b78bbbcdf2a6c821
Pablo Gomes <pablogomes_bm@hotmail.com>
2022-07-12 17:30:28 -0300

    exercicios de testes mockados com jest.fn

---
+++ modulo5/teste-automatizados-mock/testes-automatizados/src/performAttack.ts
@@ -0,0 +1,26 @@
+import { Character, validateCharacter } from "./validateCharacter";
+
+/* export const performAttack = (attacker:Character ,defender:Character)=>{
+if(!validateCharacter(attacker)|| !validateCharacter(defender)){
+    throw new Error("Invalid Character")
+};
+
+if(attacker.strength > defender.defense){
+defender.life -= attacker.strength - defender.defense
+}
+
+} */
+
+export const performAttack = (attacker: Character,defender: Character,
+    validator: (input: Character) => boolean
+  ) => {
+    if (!validator(attacker) || !validator(defender)) {
+      throw new Error("Invalid character");
+    }
+  
+    if (attacker.strength > defender.defense) {
+      defender.life -= attacker.strength - defender.defense;
+    }
+  };
+
+  // a funçao validator recebe um character e usamos ela pra validar ao inves de validarmos direto
